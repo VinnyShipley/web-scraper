37539e4abab7a56561bf0f6171a3be38533141a1
Vinny <vinnyshipley@gmail.com>
2022-08-09 16:51:52 -0700

    scraping all citation paras and printing in console

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -1,6 +1,25 @@
+from cgitb import text
 import requests
+from bs4 import BeautifulSoup
 
-
+# Declaring and pulling url content in raw
 url = 'https://en.wikipedia.org/wiki/Money'
 page = requests.get(url)
-print(page)
+
+
+# Parsing with beautiful soup
+soup = BeautifulSoup(page.content, 'html.parser')
+
+
+body_div = soup.find_all('p')
+
+para_list = []
+
+for paragraph in body_div:
+  if 'citation needed' in paragraph.text:
+    para_list.append(paragraph.text)
+
+for i in para_list:
+  print(i)
+
+
