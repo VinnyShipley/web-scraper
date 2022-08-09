f3ca4ba876e4c663e65e94334ce5f6df3bba6f3e
Vinny <vinnyshipley@gmail.com>
2022-08-09 16:57:13 -0700

    functions written and comments inserted

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -10,16 +10,22 @@ page = requests.get(url)
 # Parsing with beautiful soup
 soup = BeautifulSoup(page.content, 'html.parser')
 
-
+# finds all the paragraphs in article
 body_div = soup.find_all('p')
 
+
+# empty list to populate with citation needed paragraphs
 para_list = []
 
+# filters out non citation needed paragraphs
 for paragraph in body_div:
   if 'citation needed' in paragraph.text:
     para_list.append(paragraph.text)
 
-for i in para_list:
-  print(i)
 
+def get_citations_needed_count():
+  return print(len(para_list))
 
+def get_citation_needed_report()
+  for i in para_list:
+    print(i)
