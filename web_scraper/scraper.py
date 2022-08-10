b1bf29eb6edac1dba67e66f902ac2604438a8f00
Vinny <vinnyshipley@gmail.com>
2022-08-09 17:33:36 -0700

    functions

--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -2,30 +2,51 @@ from cgitb import text
 import requests
 from bs4 import BeautifulSoup
 
-# Declaring and pulling url content in raw
-url = 'https://en.wikipedia.org/wiki/Money'
-page = requests.get(url)
+# Gives the readout of all the paragraphs
+def get_citations_needed_report(url):
+  page = requests.get(url)
 
 
-# Parsing with beautiful soup
-soup = BeautifulSoup(page.content, 'html.parser')
+  # Parsing with beautiful soup
+  soup = BeautifulSoup(page.content, 'html.parser')
 
-# finds all the paragraphs in article
-body_div = soup.find_all('p')
-
-
-# empty list to populate with citation needed paragraphs
-para_list = []
-
-# filters out non citation needed paragraphs
-for paragraph in body_div:
-  if 'citation needed' in paragraph.text:
-    para_list.append(paragraph.text)
+  # finds all the paragraphs in article
+  body_div = soup.find_all('p')
 
 
-def get_citations_needed_count():
-  return print(len(para_list))
+  # empty list to populate with citation needed paragraphs
+  para_list = []
 
-def get_citation_needed_report():
+  # filters out non citation needed paragraphs
+  for paragraph in body_div:
+    if 'citation needed' in paragraph.text:
+      para_list.append(paragraph.text)
   for i in para_list:
     print(i)
+
+
+
+
+
+
+
+# Gives the count of the citation needed 
+def get_citation_needed_count(url):
+  page = requests.get(str(url))
+
+
+  # Parsing with beautiful soup
+  soup = BeautifulSoup(page.content, 'html.parser')
+
+  # finds all the paragraphs in article
+  body_div = soup.find_all('p')
+
+
+  # empty list to populate with citation needed paragraphs
+  para_list = []
+
+  # filters out non citation needed paragraphs
+  for paragraph in body_div:
+    if 'citation needed' in paragraph.text:
+      para_list.append(paragraph.text)
+  return print(len(para_list))
